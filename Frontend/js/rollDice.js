faa2eeea1497cc7772452af73ee5e7223e155276
WJVincent <william@william-vincent.dev>
2023-06-03 23:46:14 -0400

    added options for return value details

--- Frontend/js/rollDice.js
+++ Frontend/js/rollDice.js
@@ -1,36 +1,46 @@
-const rollDice = (sides, times) => {
+const rollDice = (sides, opts={}) => {
   let results = [];
+
+  if(!opts.times) opts.times = 1;
+  if(!opts.verbose) opts.verbose = false;
+  
+  const {times, verbose} = opts;
 
   for(let i = 0; i < times; i++){
     let num = Math.floor(Math.random() * sides + 1);
     results.push(num);
   }
 
-  return {
+  let total = results.reduce((sum, num) => sum + num, 0)
+
+  return verbose ? {
     results,
-    total: results.reduce((sum, num) => sum + num, 0)
-  }   
+    total
+  } : total; 
 };
 
-const savingThrow = (min, mod, adv) => {
-  const opts = {
-    true: () => Math.max(...rollDice(20, 2).results) + mod,
-    false: () => Math.min(...rollDice(20, 2).results) + mod,
-    undefined: () => rollDice(20, 1).total + mod
+const savingThrow = (min, mod, opts={}) => {
+  if(!opts.verbose) opts.verbose = false;
+
+  const fn = {
+    true: () => Math.max(...rollDice(20,  {times: 2, verbose: true}).results) + mod,
+    false: () => Math.min(...rollDice(20, {times: 2, verbose: true}).results) + mod,
+    undefined: () => rollDice(20) + mod
   }
 
-  let res = opts[adv]();
-  return {
+  let res = fn[opts.adv]();
+
+  return opts.verbose ? {
     msg: `roll: ${res}, DC: ${min}`,
     result: res >= min
-  }
+  } : res;
 }
 
 const rollAttributes = () => {
   let results = [];
 
   for(let i = 0; i < 6; i ++){
-    let resArr = rollDice(6, 4).results;
+    let resArr = rollDice(6, {times: 4, verbose: true}).results;
     let idx = resArr.indexOf(Math.min(...resArr));
 
     resArr.splice(idx, 1);
@@ -39,5 +49,4 @@ const rollAttributes = () => {
     results.push(res);
   }
   return results;
-}
-
+};
