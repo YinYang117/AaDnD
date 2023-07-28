9ae46a199ecab96244eeb0857b16cc8f3b25e1be
yinyang117 <slcryan2010@gmail.com>
2023-07-27 22:52:56 -0600

    working on auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,12 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+/* Register / Signup User */
+export const register = async (req, res) => {
+	try {
+		const { name, gamertag, email, password, userAvatar}
+	} catch (e) {
+
+	}
+}
--- server/models/User.js
+++ server/models/User.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
 	{
-		firstName: {type: String, required: true, min: 2, max: 30,},
-		lastName: {type: String, required: true, min: 2, max: 30,},
+		name: {type: String, required: true, min: 2, max: 30,},
+		gamertag: {type: String, min:2, max: 30},
 		email: {type: String, required: true, min: 5, max: 100, unique: true,},
 		password: {type: String, required: true, min: 5,},
 		userAvatar: {type: String, default: "",},
 		characters: {type: Array, default: [],},
 	},
-	{ timestamps: true }
-)
+	{ timestamps: true },
+);
 
-const User = mongoose.model("User", UserSchema)
+const User = mongoose.model("User", UserSchema);
+export default User;
