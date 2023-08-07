64eb1aa68f2da27c038a0197369ff98397add1c6
yinyang117 <slcryan2010@gmail.com>
2023-08-07 17:43:09 -0600

    added more to register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,8 +5,17 @@ import User from "../models/User.js";
 /* Register / Signup User */
 export const register = async (req, res) => {
 	try {
-		const { name, gamertag, email, password, userAvatar}
-	} catch (e) {
+		const { name, gamertag, email, password, userAvatar, characters } = req.body // req from frontend will have these
+		const salt = await bcrypt.genSalt();
+		const passwordHash = await bcrypt.hash(password, salt);
 
+		const newUser = new User({
+			name, gamertag, email, password: passwordHash, userAvatar, characters
+		});
+
+		const savedUser = await newUser.save();
+		res.status(201).json(savedUser) // 201 created. return json version to frontend
+	} catch (e) {
+		res.status(500).json({ error: e.message })
 	}
 }
