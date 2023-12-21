ff046477e9148b3ff693652fd080af57d9c07bc2
Ryan Schneider <slcryan2010@yahoo.com>
2023-12-21 11:26:22 -0700

    notes on sequlize

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Routes } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
@@ -17,14 +17,14 @@ function App() {
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
-        <Switch>
+        <Routes>
           <Route path='/'>
             <Splash />
           </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-        </Switch>
+        </Routes>
       )}
     </>
   );
